4342c09a96d5f70239ac66e5669f9e0bd67e05c0
Dmitriy Suzdorf <d.suzdorf@gmail.com>
2018-07-06 23:31:56 +0300

    Server routing added

---
+++ TaskManager.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace TaskManager.WebApi
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new {id = RouteParameter.Optional});
+
+            config.Routes.MapHttpRoute(
+                "Default",
+                "{*anything}",
+                new {controller = "Home", action = "Index"}
+            );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
