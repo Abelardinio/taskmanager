dc5b9fb186d9755a766b69cc4b545133a05712fa
Dmitriy Suzdorf <d.suzdorf@gmail.com>
2018-07-15 11:33:01 +0300

    Unused files

--- TaskManager.WebApi/Static/Scripts/init.js
+++
@@ -1,9 +0,0 @@
-﻿var ui = {
-    components: {},
-    controllers: {},
-    services: {}
-};
-
-ui.taskManager = (function(angular) {
-    return angular.module('TaskManager', ['ngRoute', 'cgNotify']);
-})(angular);
