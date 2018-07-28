abd6823c7b06bfc6944823f00a8871007e4f8e42
Dmitriy Suzdorf <d.suzdorf@gmail.com>
2018-07-28 16:22:59 +0300

    TaskService refactoring, BaseService added

---
+++ TaskManager.Client/src/app/services/BaseService.ts
@@ -0,0 +1,28 @@
+import { HttpHeaders } from "../../../node_modules/@angular/common/http";
+import { Observable } from "../../../node_modules/rxjs";
+import { NotificationsService } from "../../../node_modules/angular2-notifications";
+
+/**
+ * Base class for common functionality of http requests
+ */
+export abstract class BaseService {
+    protected headers: HttpHeaders;
+    protected abstract notifications: NotificationsService;
+    
+    constructor() {        
+        this.headers = new HttpHeaders();
+        this.headers = this.headers.append('Content-Type', 'application/json');
+    }
+    
+    /**
+     * Http request error handler which notifies a user an error occured and rethrows that error
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    protected handleError<T>(operation = 'operation', result?: T) {
+        return (data: any): Observable<T> => {
+            this.notifications.error(data.error.Message);
+            throw data;
+        };
+    }
+}
--- TaskManager.Client/src/app/services/TaskService.ts
+++ TaskManager.Client/src/app/services/TaskService.ts
@@ -4,32 +4,40 @@ import { NotificationsService } from 'angular2-notifications';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Messages } from '../resources/messages';
+import { BaseService } from './BaseService';
 
 @Injectable({
     providedIn: 'root',
 })
 
-export class TaskService {
-
-    private _headers: HttpHeaders;
-
+/**
+ * Provides functionality for api/task methods calls
+ */
+export class TaskService  extends BaseService{
 
     constructor(
-        private _notifications: NotificationsService,
-        private _httpClient: HttpClient) {
-        this._headers = new HttpHeaders();
-        this._headers = this._headers.append('Content-Type', 'application/json');
+        protected notifications: NotificationsService,
+        private _httpClient: HttpClient) {            
+        super();
     }
 
+    /**
+     * Returns an observable of http post method which adds new task
+     * @param taskInfo - information about task
+     */
     public Add(taskInfo: TaskInfo): Observable<Object> {
-        return this._httpClient.post(environment.API_URL + '/task', JSON.stringify(taskInfo), { headers: this._headers })
-                               .pipe(tap(() => { this._notifications.success(Messages.Tasks.Added) }),
+        return this._httpClient.post(environment.API_URL + '/task', JSON.stringify(taskInfo), { headers: this.headers })
+                               .pipe(tap(() => { this.notifications.success(Messages.Tasks.Added) }),
                                      catchError(this.handleError()));
     }
 
+    /**
+     * Returns an observable of http get method which returns a collection of task
+     * @param filter - task filter
+     */
     public Get(filter: TaskFilter): Observable<Object> {
         let params = new HttpParams();
         params = params.append('taskId', filter.taskId.toString());
@@ -37,33 +45,27 @@ export class TaskService {
         params = params.append('type', filter.type.toString());
 
 
-        return this._httpClient.get(environment.API_URL + '/task', { headers: this._headers, params: params })
+        return this._httpClient.get(environment.API_URL + '/task', { headers: this.headers, params: params })
                                .pipe(catchError(this.handleError()));
     }
 
+    /**
+     * Returns an observable of http put method which changes task status to 'Completed'
+     * @param taskId - task Id
+     */
     public Complete(taskId: number): Observable<Object> {
         return this._httpClient.put(environment.API_URL + '/task/' + taskId + '/complete', {})
-                               .pipe(tap(() => { this._notifications.success(Messages.Tasks.Completed) }),
+                               .pipe(tap(() => { this.notifications.success(Messages.Tasks.Completed) }),
                                      catchError(this.handleError()));
     }
-
-    public Delete(taskId: number): Observable<Object> {
-        return this._httpClient.delete(environment.API_URL + '/task/' + taskId, {})
-                               .pipe(tap(() => { this._notifications.success(Messages.Tasks.Removed) }),
-                                     catchError(this.handleError()));
-    }
-
 
     /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
+     * Returns an observable of http delete method which changes task status to 'Deleted'
+     * @param taskId - task Id
      */
-    private handleError<T>(operation = 'operation', result?: T) {
-        return (data: any): Observable<T> => {
-            this._notifications.error(data.error.Message);
-            throw data;
-        };
+    public Delete(taskId: number): Observable<Object> {
+        return this._httpClient.delete(environment.API_URL + '/task/' + taskId, {})
+                               .pipe(tap(() => { this.notifications.success(Messages.Tasks.Removed) }),
+                                     catchError(this.handleError()));
     }
 }
