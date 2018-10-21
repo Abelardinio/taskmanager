b07b81432e128f2f24f30e437af33f3acaa53b1c
Dmitriy Suzdorf <dmitriy.suzdorf@arcadia.spb.ru>
2018-10-21 16:07:56 +0300

    F1: Standard table
    * T13: TS Unit tests
    * Search component tests

--- TaskManager.Client/src/app/components/common/search/search.component.spec.ts
+++ TaskManager.Client/src/app/components/common/search/search.component.spec.ts
@@ -1,11 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { SearchComponent } from './search.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  const value = "value";
+  const emptyValue = "";
+
+  const testComponentInput = (input) =>{
+    component.value = input;
+    fixture.detectChanges();
+
+    return fixture.whenStable().then(() => {
+      expect(fixture.debugElement.query(By.css('input')).nativeElement.value).toBe(input);
+    });
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,7 +32,17 @@ describe('SearchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have render value in input element after its input value change', () => {
+    expect(fixture.debugElement.query(By.css('input')).nativeElement.value).toBe(emptyValue);
+    testComponentInput(value).then(() => { testComponentInput(emptyValue); });
   });
+
+  it('should change its input value after value changes in input element', fakeAsync(() => {
+    const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputElement.value = value;
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    tick(400);
+    expect(component.value).toBe(value);
+  }));
 });
