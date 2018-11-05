e285eb56d4890e95e74291fb45ea3a239a220e8e
Dmitriy Suzdorf <dmitriy.suzdorf@arcadia.spb.ru>
2018-11-05 18:55:13 +0300

    F2: Real time table update
    * T18: Connect "MessagingService" and "WebApi" through rabbit MQ
    * Ninject integration with aspnet core

--- TaskManager.MessagingService/DependencyConfig.cs
+++ TaskManager.MessagingService/DependencyConfig.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Ninject;
+using Ninject.Activation;
 using TaskManager.Core.ConnectionContext;
 using TaskManager.Core.EventAccessors;
 using TaskManager.ServiceBus;
@@ -8,9 +10,14 @@ namespace TaskManager.MessagingService
 {
     public static class DependencyConfig
     {
-        public static void Configure(IServiceCollection services)
+        public static void Configure(IKernel kernel, Func<IContext,object> requestScope)
         {
-            services.AddSingleton<IConnectionSettings, AppSettings.AppSettings>();
+            kernel.Bind<IConnectionSettings>().To<AppSettings.AppSettings>();
+            kernel.Bind<ITaskEventAccessor>().To<TaskEventAccessor>();
+            kernel.Bind<IChannelStorage, IEventScopeFactory>().To<ChannelFactory>().InSingletonScope();
+            kernel.Bind<IConnectionStorage, IConnectionFactory>().To<ServiceBusConnectionFactory>().InSingletonScope();
+            kernel.Bind<IServiceBusClient>().To<ServiceBusClient>();
+            kernel.Bind<IMessageSerializer>().To<MessageSerializer>();
         }
     }
 }
--- TaskManager.MessagingService/Startup.cs
+++ TaskManager.MessagingService/Startup.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Threading;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Ninject;
+using Ninject.Activation;
+using Ninject.Infrastructure.Disposal;
+using TaskManager.Common.AspNetCore;
+using TaskManager.Core.ConnectionContext;
+using TaskManager.Core.EventAccessors;
+using TaskManager.Core.Messages;
+using TaskManager.MessagingService.AppSettings;
 using TaskManager.ServiceBus;
 
 namespace TaskManager.MessagingService
 {
     public class Startup
     {
+        private readonly AsyncLocal<Scope> _scopeProvider = new AsyncLocal<Scope>();
+        private IOptions<AppSettings.AppSettingsModel> _settings;
+        private IKernel Kernel { get; set; }
+
+        private object Resolve(Type type) => Kernel.Get(type);
+        private object RequestScope(IContext context) => _scopeProvider.Value;
+
         public Startup(IConfiguration configuration)
         {
             AppConfiguration = configuration;
@@ -20,7 +40,9 @@ namespace TaskManager.MessagingService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddOptions();
+
 
             services.Configure<IISOptions>(options =>
             {
@@ -29,16 +51,22 @@ namespace TaskManager.MessagingService
 
             services.Configure<AppSettings.AppSettingsModel>(AppConfiguration);
 
+            _settings = services.BuildServiceProvider().GetService<IOptions<AppSettings.AppSettingsModel>>();
+
             services.AddSignalR();
 
-            DependencyConfig.Configure(services);
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            var settings = services.BuildServiceProvider().GetService<IConnectionSettings>();
+            services.AddRequestScopingMiddleware(() => _scopeProvider.Value = new Scope());
+            services.AddCustomControllerActivation(Resolve);
+            services.AddCustomViewComponentActivation(Resolve);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            Kernel = RegisterApplicationComponents(app, loggerFactory);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -48,6 +76,44 @@ namespace TaskManager.MessagingService
             {
                 routes.MapHub<TasksHub>("/tasks");
             });
+
+            Kernel.Get<IConnectionFactory>().Create();
+            Kernel.Get<IEventScopeFactory>().Create();
+            var taskEventAccessor = Kernel.Get<ITaskEventAccessor>();
+            taskEventAccessor.OnStatusUpdated(OnStatusUpdated);
         }
+
+        public void OnStatusUpdated(ITaskStatusUpdatedMessage message)
+        {
+
+        }
+
+        private IKernel RegisterApplicationComponents(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            // IKernelConfiguration config = new KernelConfiguration();
+            Kernel = new StandardKernel();
+
+            // Register application services
+            foreach (var ctrlType in app.GetControllerTypes())
+            {
+                Kernel.Bind(ctrlType).ToSelf().InScope(RequestScope);
+            }
+
+            // Cross-wire required framework services
+            Kernel.BindToMethod(app.GetRequestService<IViewBufferScope>);
+            Kernel.Bind<ILoggerFactory>().ToConstant(loggerFactory);
+            Kernel.Bind<IOptions<AppSettingsModel>>().ToMethod(context => _settings);
+
+            DependencyConfig.Configure(Kernel, RequestScope);
+
+            return Kernel;
+        }
+
+        private sealed class Scope : DisposableObject { }
+    }
+
+    public static class BindingHelpers
+    {
+        public static void BindToMethod<T>(this IKernel config, Func<T> method) => config.Bind<T>().ToMethod(c => method());
     }
 }
