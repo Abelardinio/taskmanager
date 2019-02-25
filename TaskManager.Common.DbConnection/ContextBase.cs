467869cadc92119eafb1350b9442ad04ae488ff2
Dmitriy Suzdorf <d.suzdorf@gmail.com>
2019-02-26 00:03:32 +0300

    F7: Authorization and Authentication service [Users page]
    * T124: Create new application and database
    * Move DbConnection common functionality to common library

---
+++ TaskManager.Common.DbConnection/ContextBase.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using TaskManager.Core.ConnectionContext;
+
+namespace TaskManager.Common.DbConnection
+{
+    public abstract class ContextBase : DbContext, IDatabaseScope
+    {
+        private readonly string _connectionString;
+        private readonly bool _isInTransactionScope;
+        private readonly IDbContextTransaction _transaction;
+
+        public bool IsDisposed { get; private set; }
+
+        protected ContextBase(string connectionString, bool isInTransactionScope)
+        {
+            _isInTransactionScope = isInTransactionScope;
+            _connectionString = connectionString;
+
+            if (isInTransactionScope)
+            {
+                _transaction = Database.BeginTransaction();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString, o => o.UseRowNumberForPaging());
+            }
+        }
+
+        public void Commit()
+        {
+            if (_isInTransactionScope)
+            {
+                _transaction.Commit();
+            }
+        }
+
+        public void Rollback()
+        {
+            if (_isInTransactionScope)
+            {
+                _transaction.Rollback();
+            }
+        }
+
+        public override void Dispose()
+        {
+            if (_isInTransactionScope)
+            {
+                _transaction.Dispose();
+            }
+
+            base.Dispose();
+            IsDisposed = true;
+        }
+    }
+}
--- TaskManager.DbConnection/Context.cs
+++ TaskManager.DbConnection/Context.cs
@@ -1,64 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
-using TaskManager.Core.ConnectionContext;
+using TaskManager.Common.DbConnection;
 using TaskManager.DbConnection.Entities;
-using DbContext = Microsoft.EntityFrameworkCore.DbContext;
 
 namespace TaskManager.DbConnection
 {
-    public class Context : DbContext, IDatabaseScope
+    public class Context : ContextBase
     {
-        private readonly string _connectionString;
-        private readonly bool _isInTransactionScope;
-        private readonly IDbContextTransaction _transaction;
-
-        public Context(string connectionString, bool isInTransactionScope)
+        public Context(string connectionString, bool isInTransactionScope) : base (connectionString, isInTransactionScope)
         {
-            _isInTransactionScope = isInTransactionScope;
-            _connectionString = connectionString;
-
-            if (isInTransactionScope)
-            {
-                _transaction = Database.BeginTransaction();
-            }
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(_connectionString, o => o.UseRowNumberForPaging());
-            }
         }
 
         public virtual DbSet<TaskEntity> Tasks { get; set; }
-
-        public bool IsDisposed { get; private set; }
-
-        public void Commit()
-        {
-            if (_isInTransactionScope)
-            {
-                _transaction.Commit();
-            }
-        }
-
-        public void Rollback()
-        {
-            if (_isInTransactionScope)
-            {
-                _transaction.Rollback();
-            }
-        }
-
-        public override void Dispose()
-        {
-            if (_isInTransactionScope)
-            {
-                _transaction.Dispose();
-            }
-
-            base.Dispose();
-            IsDisposed = true;
-        }
     }
 }
