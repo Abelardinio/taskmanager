8a2751aee927dee348e13cb4b57ebfbdec8262b0
Dmitriy Suzdorf <d.suzdorf@gmail.com>
2019-03-03 21:50:04 +0300

    F7: Authorization and Authentication service [Users page]
    * T130: Update developer's deployment configuration

--- TaskManager.Deployment/install.cmd
+++ TaskManager.Deployment/install.cmd
@@ -1,10 +1,10 @@
-sqlcmd -b -v DATABASE_NAME="TaskManager" USER_NAME="TaskManagerUser" PASSWORD="nNx4vp3R3vsh8$d2" -i "CreateDatabaseAndUser.sql"
-sqlcmd -b -v DATABASE_NAME="TaskManagerAuth" USER_NAME="TaskManagerAuthUser" PASSWORD="Vyk5ALNvGRNnCKQB" -i "CreateDatabaseAndUser.sql"
+REM sqlcmd -b -v DATABASE_NAME="TaskManager" USER_NAME="TaskManagerUser" PASSWORD="nNx4vp3R3vsh8$d2" -i "CreateDatabaseAndUser.sql"
+REM sqlcmd -b -v DATABASE_NAME="TaskManagerAuth" USER_NAME="TaskManagerAuthUser" PASSWORD="Vyk5ALNvGRNnCKQB" -i "CreateDatabaseAndUser.sql"
 
 powershell -executionPolicy bypass -file "iis.ps1"
 
-echo.  >> %WINDIR%\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 taskmanager >> %WINDIR%\System32\Drivers\Etc\Hosts
+REM echo.  >> %WINDIR%\System32\Drivers\Etc\Hosts
+REM echo 127.0.0.1 taskmanager >> %WINDIR%\System32\Drivers\Etc\Hosts
 cd ..\TaskManager.Deployment
 
 call build FolderProfile dist/task-manager-client
