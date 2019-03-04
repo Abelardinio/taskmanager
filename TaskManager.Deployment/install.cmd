2457fef156fab5cfde348fd3b8e4502485d61033
Dmitriy Suzdorf <dmitriy.suzdorf#arcadia.spb.ru>
2019-03-04 13:26:34 +0300

    F7: Authorization and Authentication service [Users page]
    * Commented rows reverted

--- TaskManager.Deployment/install.cmd
+++ TaskManager.Deployment/install.cmd
@@ -1,10 +1,10 @@
-REM sqlcmd -b -v DATABASE_NAME="TaskManager" USER_NAME="TaskManagerUser" PASSWORD="nNx4vp3R3vsh8$d2" -i "CreateDatabaseAndUser.sql"
-REM sqlcmd -b -v DATABASE_NAME="TaskManagerAuth" USER_NAME="TaskManagerAuthUser" PASSWORD="Vyk5ALNvGRNnCKQB" -i "CreateDatabaseAndUser.sql"
+sqlcmd -b -v DATABASE_NAME="TaskManager" USER_NAME="TaskManagerUser" PASSWORD="nNx4vp3R3vsh8$d2" -i "CreateDatabaseAndUser.sql"
+sqlcmd -b -v DATABASE_NAME="TaskManagerAuth" USER_NAME="TaskManagerAuthUser" PASSWORD="Vyk5ALNvGRNnCKQB" -i "CreateDatabaseAndUser.sql"
 
 powershell -executionPolicy bypass -file "iis.ps1"
 
-REM echo.  >> %WINDIR%\System32\Drivers\Etc\Hosts
-REM echo 127.0.0.1 taskmanager >> %WINDIR%\System32\Drivers\Etc\Hosts
+echo.  >> %WINDIR%\System32\Drivers\Etc\Hosts
+echo 127.0.0.1 taskmanager >> %WINDIR%\System32\Drivers\Etc\Hosts
 cd ..\TaskManager.Deployment
 
 call build FolderProfile dist/task-manager-client
