ed82111d1789ba2ede60f57dad546f589e09f8d3
Dmitriy Suzdorf <dmitriy.suzdorf#arcadia.spb.ru>
2019-04-28 16:09:06 +0300

    F17: Permissions
    * T160: Add UI logic for projects lookup grid

--- TaskManager.Client/src/app/components/modals/project-permission-modal/project-permission-modal.component.ts
+++ TaskManager.Client/src/app/components/modals/project-permission-modal/project-permission-modal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { ProjectService } from 'src/app/services/ProjectService';
+import { Lookup } from 'src/app/models/Lookup';
 
 @Component({
   selector: 'app-project-permission-modal',
@@ -6,16 +9,32 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./project-permission-modal.component.css']
 })
 export class ProjectPermissionModalComponent implements OnInit {
-  @Input() public isVisible = false;
+  @Input() public set isVisible(value: boolean) {
+    if (value) {
+      this._projectService.GetLookup().subscribe(projects => this.projectsArray = projects);
+    }
+
+    this.isModalWindowVisible = value;
+  }
+
+  @Input() public user: User;
   @Output() public isVisibleChange = new EventEmitter();
-  public constructor() { }
+  public projectsArray: Lookup[];
+  public isModalWindowVisible = false;
+  public selected: any = {};
+
+  public constructor(private _projectService: ProjectService) { }
 
   public ngOnInit() {
   }
 
   public onCancelClick() {
-    this.isVisible = false;
+    this.isModalWindowVisible = false;
     this.isVisibleChange.emit();
+  }
+
+  public onProjectSelect(project: Lookup) {
+    this.selected = project;
   }
 
   public onOkClick() {
