3af4bada29007d48046f187060f24da8dd06f06a
Dmitriy Suzdorf <dmitriy.suzdorf#arcadia.spb.ru>
2019-05-05 13:14:06 +0300

    F17: Permissions
    * T172: Modify developer and docker configuration

--- TaskManager.Deployment/install.cmd
+++ TaskManager.Deployment/install.cmd
@@ -1,5 +1,6 @@
 sqlcmd -b -v DATABASE_NAME="TaskManager" USER_NAME="TaskManagerUser" PASSWORD="nNx4vp3R3vsh8$d2" -i "CreateDatabaseAndUser.sql"
 sqlcmd -b -v DATABASE_NAME="TaskManagerAuth" USER_NAME="TaskManagerAuthUser" PASSWORD="Vyk5ALNvGRNnCKQB" -i "CreateDatabaseAndUser.sql"
+sqlcmd -b -v DATABASE_NAME="TaskManagerMessaging" USER_NAME="TaskManagerMessagingUser" PASSWORD="k83pDAHGzrX4GULB" -i "CreateDatabaseAndUser.sql"
 
 powershell -executionPolicy bypass -file "iis.ps1"
 
